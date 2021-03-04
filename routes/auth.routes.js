6aa7fcb38a5d9d741b1e8fd2a351441719dd7279
damla <damlatiltak@gmail.com>
2021-03-04 14:41:35 +0100

    nothing

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,16 +8,16 @@ const OwnerModel = require('../models/Owner.model')
 //POST ROUTE SIGNUP
 
 router.post('/signup', (req, res, next) => {
-  const {email, password, type} = req.body;
+  const { email, password, type } = req.body;
 
 
   // SERVER SIDE VALIDATION
-  if(!email || ! password){
+  if (!email || !password) {
     res.status(500)
       .json({
         errorMessage: 'Please enter email and password'
       })
-      return
+    return
   }
   // const myRegex = new RegExp(/^[a-z0-9](?!.*?[^\na-z0-9]{2})[^\s@]+@[^\s@]+\.[^\s@]+[a-z0-9]$/);
   //   if (!myRegex.test(email)) {
@@ -35,11 +35,11 @@ router.post('/signup', (req, res, next) => {
   //   }
 
   //PassWord Hasing
-  
+
   let salt = bcrypt.genSaltSync(10);
-    let hash = bcrypt.hashSync(password, salt);
-    if(type === 'musician'){
-      MusicianModel.create({email, password: hash})
+  let hash = bcrypt.hashSync(password, salt);
+  if (type === 'musician') {
+    MusicianModel.create({ email, password: hash })
       .then((user) => {
         // ensuring that we don't share the hash as well with the user
         user.password = "***";
@@ -52,7 +52,7 @@ router.post('/signup', (req, res, next) => {
             errorMessage: 'email entered already exists!',
             message: err,
           });
-        } 
+        }
         else {
           res.status(500).json({
             errorMessage: 'Something went wrong! Go to sleep!',
@@ -60,9 +60,9 @@ router.post('/signup', (req, res, next) => {
           });
         }
       })
-    }
-    else{
-      OwnerModel.create({email, password: hash})
+  }
+  else {
+    OwnerModel.create({ email, password: hash })
       .then((user) => {
         // ensuring that we don't share the hash as well with the user
         user.password = "***";
@@ -75,7 +75,7 @@ router.post('/signup', (req, res, next) => {
             errorMessage: 'email entered already exists!',
             message: err,
           });
-        } 
+        }
         else {
           res.status(500).json({
             errorMessage: 'Something went wrong! Go to sleep!',
@@ -83,72 +83,72 @@ router.post('/signup', (req, res, next) => {
           });
         }
       })
-    }
+  }
 
 
 })
 
 // POST ROUTE FOR SIGNIN
 router.post('/signin', (req, res, next) => {
-  const{email, password} = req.body;
+  const { email, password } = req.body;
   // -----SERVER SIDE VALIDATION ----------
-    /*
-    if ( !email || !password) {
-        res.status(500).json({
-            error: 'Please enter Username. email and password',
-       })
-      return;  
-    }
-    const myRegex = new RegExp(/^[a-z0-9](?!.*?[^\na-z0-9]{2})[^\s@]+@[^\s@]+\.[^\s@]+[a-z0-9]$/);
-    if (!myRegex.test(email)) {
-        res.status(500).json({
-            error: 'Email format not correct',
-        })
-        return;  
-    }
-    */
-  
-    MusicianModel.findOne({email})
-      .then((user) => {
-        // checking for password match
-        bcrypt.compare(password, user.password)
-          .then((doesItMatch) => {
-            if(doesItMatch){
-              user.password = '***';
-              req.session.loggedInUser = user;
-              res.status(200).json(user)
-            }
-            else {
-              res.status(500).json({
-                  error: 'Passwords don\'t match',
-              })
-            return; 
-          }
-          })
-          .catch((err) => {
-            res.status(500).json({
-              error: err
-            })
-            return
-          });
+  /*
+  if ( !email || !password) {
+      res.status(500).json({
+          error: 'Please enter Username. email and password',
+     })
+    return;  
+  }
+  const myRegex = new RegExp(/^[a-z0-9](?!.*?[^\na-z0-9]{2})[^\s@]+@[^\s@]+\.[^\s@]+[a-z0-9]$/);
+  if (!myRegex.test(email)) {
+      res.status(500).json({
+          error: 'Email format not correct',
       })
-      .catch((err) => {
-        OwnerModel.findOne({email})
+      return;  
+  }
+  */
+
+  MusicianModel.findOne({ email })
+    .then((user) => {
+      // checking for password match
+      bcrypt.compare(password, user.password)
+        .then((doesItMatch) => {
+          if (doesItMatch) {
+            user.password = '***';
+            req.session.loggedInUser = user;
+            res.status(200).json(user)
+          }
+          else {
+            res.status(500).json({
+              error: 'Passwords don\'t match',
+            })
+            return;
+          }
+        })
+        .catch((err) => {
+          res.status(500).json({
+            error: err
+          })
+          return
+        });
+    })
+    .catch((err) => {
+      OwnerModel.findOne({ email })
         .then((user) => {
           // checking for password match
           bcrypt.compare(password, user.password)
             .then((doesItMatch) => {
-              if(doesItMatch){
+              if (doesItMatch) {
                 user.password = '***';
                 req.session.loggedInUser = user;
                 res.status(200).json(user)
               }
               else {
                 res.status(500).json({
-                    error: 'Passwords don\'t match',
+                  error: 'Passwords don\'t match',
                 })
-              return; 
-            }
+                return;
+              }
             })
             .catch((err) => {
               res.status(500).json({
@@ -157,14 +157,14 @@ router.post('/signin', (req, res, next) => {
               return
             });
         })
-        .catch(()=>{
+        .catch(() => {
           res.status(500).json({
             error: 'Wrong email',
             message: err
           })
           return;
         })
-      })
+    })
 })
 
 router.post('/signout', (req, res, next) => {
@@ -173,10 +173,10 @@ router.post('/signout', (req, res, next) => {
 })
 
 const isLoggedIn = (req, res, next) => {
-  if(req.session.loggedInUser) {
+  if (req.session.loggedInUser) {
     next()
   }
-  else{
+  else {
     res.status(401).json({
       message: 'Unauthorized user',
       code: 401
