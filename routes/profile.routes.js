f17a5253d15d1ff73c746d546435eb1036e7dc05
damla <damlatiltak@gmail.com>
2021-03-07 14:42:34 +0100

    get, edit and delete venues done

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const MusicianModel = require('../models/Musician.model')
 const OwnerModel = require('../models/Owner.model')
+const VenueModel = require('../models/Venue.model')
 
 
 router.get('/users', (req, res) => {
@@ -17,6 +18,16 @@ router.get('/users', (req, res) => {
     })
 })
 
+
+router.get('/venues', (req, res, next) => {
+  VenueModel.find()
+    .then((venues) => {
+      console.log("all venues: ", venues)
+      res.status(200).json(venues);
+    }).catch((err) => {
+
+    });
+})
 
 router.get('/musician-profile/:userId', (req, res) => {
 
@@ -76,6 +87,105 @@ router.patch('/owner-profile/:ownerId', (req, res) => {
 
 })
 
+
+router.post('/add-venue', (req, res, next) => {
+  const { title, location, size, ownerId } = req.body;
+  // let owner = req.session.loggedInUser;
+
+  // SERVER SIDE VALIDATION
+  if (!title || !location || !size) {
+    res.status(500)
+      .json({
+        message: 'Please fill all the fields'
+      })
+    return
+  }
+
+  VenueModel.create({ title, location, size, ownerId })
+    .then((venue) => {
+      res.status(200).json(venue);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "Venue creation failed",
+        message: err
+      });
+    });
+})
+
+
+router.get('/venues/:ownerId', (req, res, next) => {
+  const { ownerId } = req.params;
+
+  VenueModel.find({ ownerId })
+    .populate("ownerId")
+    .then((venues) => {
+      res.status(200).json(venues);
+    })
+    .catch((err) => {
+      console.log("getting venues failed");
+      res.status(500).json({
+        message: err
+      })
+    });
+})
+
+
+
+router.get('/venue/:venueId', (req, res, next) => {
+  const { venueId } = req.params;
+
+  VenueModel.findById(venueId)
+    .then((venue) => {
+      console.log("venue is fetched: ", venue);
+      res.status(200).json(venue);
+    })
+    .catch((err) => {
+      console.log("can't get venue");
+      res.status(500).json({
+        message: err
+      })
+    });
+})
+
+
+router.patch('/venue/:venueId', (req, res, next) => {
+  const { venueId } = req.params;
+  const { title, location, size } = req.body;
+
+  VenueModel.findByIdAndUpdate(venueId, { title, location, size }, { new: true })
+    .then((venue) => {
+      console.log("venue edited: ", venue);
+      res.status(200).json(venue);
+    })
+    .catch((err) => {
+      console.log("edit venue failed");
+      res.status(500).json({
+        message: err
+      })
+    });
+})
+
+
+router.delete('/venue/:venueId', (req, res, next) => {
+  const { venueId } = req.params;
+  console.log("delete venue: ", venueId);
+
+  VenueModel.findByIdAndDelete(venueId)
+    .then((deletedVenue) => {
+      console.log("venue deleted: ", deletedVenue);
+      res.status(200).json(deletedVenue)
+    })
+    .catch((err) => {
+      console.log("venue couldn't be deleted");
+      res.status(500).json({
+        message: err
+      })
+    });
+
+
+})
 
 
 module.exports = router
