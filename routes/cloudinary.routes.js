942c06162221e9c5d7ce7e74f03862a52bf9d883
damla <damlatiltak@gmail.com>
2021-03-08 15:24:46 +0100

    upload image for musicians done

--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -5,7 +5,7 @@ const uploader = require('../config/cloudinary.config');
 router.post('/upload', uploader.single("imageUrl"), (req, res, next) => {
   // the uploader.single() callback will send the file to cloudinary and get you and obj with the url in return
   console.log('file is: ', req.file)
-  
+
   if (!req.file) {
     console.log("there was an error uploading the file")
     next(new Error('No file uploaded!'));
@@ -13,8 +13,9 @@ router.post('/upload', uploader.single("imageUrl"), (req, res, next) => {
   }
   // You will get the image url in 'req.file.path'
   // we send that back to the client 
-  res.status(200).json({image: req.file.path})
+  res.status(200).json({ image: req.file.path })
 })
+
 
 
 module.exports = router;
