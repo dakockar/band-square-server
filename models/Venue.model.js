42be33059fd8a3d6f623324f2c95e139493223eb
Thomas de Groot <degroot12@gmail.com>
2021-03-08 19:09:28 +0100

    changed venue model and route

--- models/Venue.model.js
+++ models/Venue.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const VenueSchema = new Schema({
   title: String,
-  imgUrl: String,
+  imgUrl: Array,
   location: String,
   size: Number,
   ownerId: { type: Schema.Types.ObjectId, ref: "Owner" },
